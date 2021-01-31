d8d028497f1ea554298d3dd7f219a27200737091
pranavkaul <pkaul7@gmail.com>
2021-01-31 14:10:48 +0530

    update

---
+++ Python Solutions/Lists.py
@@ -0,0 +1,9 @@
+'''https://www.hackerrank.com/challenges/python-lists/problem'''
+n = int(input())
+l = []
+for _ in range(n):
+	cmd = input().split()
+	if cmd[0] == 'print':
+		print(l)
+	else:
+		getattr(l, cmd[0])(*(map(int, cmd[1:])))
